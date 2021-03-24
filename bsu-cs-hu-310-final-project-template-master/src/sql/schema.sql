71f1cbe5a455eaa11ac27bedeaf5c42639725c28
SayrePeterson <77171707+SayrePeterson@users.noreply.github.com>
2021-03-24 17:14:42 -0600

    Update schema.sql

--- bsu-cs-hu-310-final-project-template-master/src/sql/schema.sql
+++ bsu-cs-hu-310-final-project-template-master/src/sql/schema.sql
@@ -30,3 +30,58 @@ CREATE TABLE IF NOT EXISTS students(
   birthdate DATE, 
   PRIMARY KEY (student_id) 
 );
+
+CREATE TABLE IF NOT EXISTS academic_titles(
+  academic_title_id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  PRIMARY KEY (academic_title_id)
+  );
+  
+CREATE TABLE IF NOT EXISTS instructors(
+  instructor_id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(80) NOT NULL,
+  last_name VARCHAR(80) NOT NULL,
+  academic_title_id INT,
+  PRIMARY KEY(instructor_id),
+  FOREIGN KEY (academic_title_id) REFERENCES academic_titles(academic_title_id)
+  );
+  
+  CREATE TABLE IF NOT EXISTS terms(
+    term_id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(80) NOT NULL,
+    PRIMARY KEY (term_id)
+    );
+    
+  CREATE TABLE IF NOT EXISTS class_sections(
+    class_section_id INT AUTO_INCREMENT NOT NULL,
+    class_id INT NOT NULL,
+    instructor_id INT NOT NULL,
+    term_id INT NOT NULL
+    PRIMARY KEY(class_section_id),
+    FOREIGN KEY (class_id) REFERENCES classes(class_id),
+    FOREIGN KEY (instructor_id) REFERENCES instructors(instructor_id),
+    FOREIGN KEY (term_id) REFERENCES terms(term_id)
+    );
+    
+  CREATE TABLE IF NOT EXISTS grades(
+    grade_id INT AUTO_INCREMENT NOT NULL,
+    letter_grade CHAR(2) NOT NULL,
+    PRIMARY KEY(grade_id)
+    );
+    
+  CREATE TABLE IF NOT EXISTS class_registrations(
+    class_registration_id INT AUTO_INCREMENT NOT NULL,
+    class_section_id INT NOT NULL,
+    student_id INT NOT NULL,
+    grade_id INT,
+    signup_timestamp datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(class_registration_id),
+    FOREIGN KEY (class_section_id) REFERENCES class_sections(class_section_id),
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (grade_id) REFERENCES grades(grade_id)
+    );
+    
+  
+  
+  
+  
